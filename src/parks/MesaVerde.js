544d42da825fb9d21a6ffdd495d129e3e3128f08
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-05-09 05:27:04 +0300

    Completed assignment

--- src/parks/MesaVerde.js
+++ src/parks/MesaVerde.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function MesaVerde() {
+function MesaVerdes() {
   return <h1>Mesa Verde National Park</h1>;
 }
 
-export default MesaVerde;
+export default MesaVerdes;
